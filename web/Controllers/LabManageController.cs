c0b6ae0565c1f3bc708d0590d6107b83e4f4daba
youcanyouupgithub <469470971@qq.com>
2017-07-23 11:21:40 +0800

    更新MVC框架版本

---
+++ web/Controllers/LabManageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    /// <summary>
+    /// 实验室管理
+    /// </summary>
+    public class LabManageController : Controller
+    {
+        /// <summary>
+        /// 实验考核
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ExperimentCheck()
+        {
+            return View();
+        }
+
+    }
+}
--- web/Global.asax.cs
+++ web/Global.asax.cs
@@ -17,11 +17,10 @@ namespace Web
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
